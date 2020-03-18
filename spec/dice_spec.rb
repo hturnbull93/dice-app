d9d3f54e5ea98c98ff76c9596f03553a43f58ec3
Harry Turnbull <hturnbull@live.co.uk>
2020-03-18 11:25:24 +0000

    #roll returns random number between 1 and 6

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -4,8 +4,13 @@ describe Dice do
 
   subject(:dice) { Dice.new }
 
-  it "responds to #rolls" do
+  it "responds to #roll" do
     expect(dice).to respond_to(:roll)
+  end
+
+  it "#roll returns a random number between 1 and 6 inclusive" do
+    expect(dice.roll).to be <= 6
+    expect(dice.roll).to be >= 1
   end
 
 end
